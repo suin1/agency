1001b69018e230e26dca33be66d77a2259c6225a
suin1 <142865315+suin1@users.noreply.github.com>
2023-11-24 17:02:08 +0900

    Update catData.js

--- src/assets/DB/catData.js
+++ src/assets/DB/catData.js
@@ -1,32 +1,33 @@
-export default [
+const catData = [
   {
     id: 1,
-    name: "오리 1",
+    name: "덕덕이",
     age: 3,
     image: "./images/duck1.jpg",
   },
   {
     id: 2,
-    name: "오리 2",
+    name: "꽉꽉이",
     age: 6,
     image: "./images/duck2.jpg",
   },
   {
     id: 3,
-    name: "오리 3",
+    name: "꽥꽥이",
     age: 1,
     image: "./images/duck3.jpg",
   },
   {
     id: 4,
-    name: "오리 4",
+    name: "덕이",
     age: 6,
     image: "./images/duck4.jpg",
   },
   {
     id: 5,
-    name: "오리 5",
+    name: "오둥이",
     age: 2,
     image: "./images/duck5.jpg",
   },
 ];
+export default catData;
